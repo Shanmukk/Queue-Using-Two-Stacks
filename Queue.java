33f6e5f5b6a06e646bd9a278f584338074f81cf4
Shanmukk <vemulakondashanmuk@msitprogram.net>
2022-05-11 15:17:31 +0530

    completed

--- Queue.java
+++ Queue.java
@@ -34,7 +34,7 @@ public class Queue{
     }
     
     // Return whether the queue is empty.
-    //Time and space complexity for this function is O(1)
+    //Time and space complexity for this function is O(1).
     public boolean empty() {
         return (stack1.isEmpty() && stack2.isEmpty());
     }
